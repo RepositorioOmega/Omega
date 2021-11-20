177088ed79b79dfc62070104dd041823987d24a1
Jasmin Fuquen <56594146+JasminFuquen@users.noreply.github.com>
2021-11-20 00:39:42 -0500

    Se actualiza para detalles de la lista

--- app/src/main/java/com/example/omegafood/DetailPlace.kt
+++ app/src/main/java/com/example/omegafood/DetailPlace.kt
@@ -19,10 +19,9 @@ class DetailPlace : AppCompatActivity() {
 
 
         val carousel: ImageCarousel = findViewById(R.id.carousel)
-        list.add(CarouselItem("https://blogger.googleusercontent.com/img/a/AVvXsEhHlO-U6OJUej6BZ6dSfu_Ea7rxpvM3PQXX0ClPpd-X-HJO76tYDBzsEMC44Rx6KWtVt_8ukgIgjy0YyJ12NfvXCqEbg8MX-PbJtal7mdd5FJKP_pQFA3ecqWYTxBirTM1bOxhVo7Jv1JJEnqN8bVPcHyOxK8jkXXkWg0Cg4u4p_y_aOz1YOAy_26xN9g=s320"))
-        list.add(CarouselItem("https://blogger.googleusercontent.com/img/a/AVvXsEjgWofmsbsb2djfDiVycXx7RzDR2Gn6FF2RkU3ruTDeIoUW9YZjc9ne1ByupuCXkq-5bcyuAdYOrjkMnCJlIwWHndpkhGWq9neutoGGdKjd4K9p1rlxEGg5NrGKuL7JwS7y8Tmdw40wK-v6Cuio3GMgltReW-xX3IQW3YFDFapSc433eVCXREzG28hL2g=s320"))
-        list.add(CarouselItem("https://blogger.googleusercontent.com/img/a/AVvXsEj2xCTRZ7_TKGMtc1C5r96YCfVZwJ7Npyj5IKQhlgT0p_DHEltDMs0l4bVDF874APUIcN7Ft_iMkfsldc4pWyZYJLY_ajEwnul3ReFIe10qotQOZtnxwPRVOx1PBHqyALoQkQIwCmW5BeLIPdCpx0UeAn6hfPlATU7BHsDEw9UxU_8oinmhG9fzwv4JvQ=s320"))
-        list.add(CarouselItem("https://blogger.googleusercontent.com/img/a/AVvXsEj08qprxBmoDYegtodCDZhvaQgCUs9GWSJjgKeLLu3C6pg3cZl991KshnOvBKWOPzENuMrNkF9dhItLP9UDwYEvsE4eAooDKu_3mFw42O8Vdz59ANZ7j51SSevkfSN3uh9QEZbjGRDI52h151DzZJWUpuSUAJBL6HzxygwRof3TntbCMDX0yvOWH6QdJw=s320"))
+        list.add(CarouselItem("https://blogger.googleusercontent.com/img/a/AVvXsEgukXJnUPZIOXr3ksR44Qx5iiZktsttEoJp6Dk0QpbQo_Md3Al8ZxTbthbEST4je6iia-jSeJQgOQ9rDJ3UyRnBr91vtp2Ks7502tL_r8aeX8Ueb_ecMRLwxWiaCDPXmxeuHMrFPYwoJCdiwXqPyKgy2PQBdGA06YbSBsU1UYaxQHhB5a2iMWlbeIB3Jg=s320"))
+        list.add(CarouselItem("https://blogger.googleusercontent.com/img/a/AVvXsEiey1RNE6yf4fXdociCZg019Z86JC0sPgmA8SZcIcoWgsTUkQjFeFScGNeYk_lgmfGq_8sBXhZ5QuFCmbC-Zx1YGc_zCe-mgufDZp6yz4q-PGqSYUhOAe_9bINsNIQH__JzOh741TdNcCltnN5xg0-ZJnyJIhPxLcGtnkp1w_Z8Q88JJOYF1Y5bI4_RMg=s320"))
+        list.add(CarouselItem("https://blogger.googleusercontent.com/img/a/AVvXsEgjAlajaE4pN037CfVTiR66g6nnnExcteC1r8jqPkTh3ybmViy1bt8fVtWfr9c5EkFXc4xkrDNr7krhTDJ9LSc0Hj1588t86-Nym8GqpR5qd8hpToszkfDbZBxXGYDaSEmM6l0uq1J7HKEOsPGS5_VoWA3xNjYQsUGJrhG0ZY_B-F8FxYwUXzBZ1Rvg0w=s320"))
         carousel.addData(list)
     }
 }
